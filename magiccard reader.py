a09c1531eb0496089cd2f43cef45783ead76e9b3
Lethe <168459476+LetheTheForgotten@users.noreply.github.com>
2024-11-26 15:47:57 +0100

    refactor

--- magiccard reader.py
+++ magiccard reader.py
@@ -16,12 +16,14 @@ while True:
         print("camera not found")
         break
     imageboxed = cv2.rectangle(image, (25, 30), (500, 70), color=(0,225,0), thickness=2)
+
     #show camera feed
     cv2.imshow('camera feed', imageboxed)
     
     #show feed until key is pressed
     key=cv2.waitKey(10)
     if key == 121 or key == 89:
+
         #crop to box
         image = image[30:70,25:500]
         cv2.imshow('cropped', image)
@@ -30,16 +32,26 @@ while True:
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         ret ,thresholded = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY +cv2.THRESH_OTSU)
         cv2.imshow('greyscale', thresholded)
-        #parse text
+
+        #parse and clean text
         text = pytesseract.image_to_string(Image.fromarray(thresholded), config='--psm 11')
         text = text.strip()
         text = re.sub(r'\W+','',text)
+
+        #if something was found, search it
         if len(text) > 0:
             print(text)
+
+            #preparing payload
             headers = {"User-Agent":"LetheCardChecker","Accept":"*/*","Content-Type": "application/json"} 
+
+            #sending payload
             response = requests.get("https://api.scryfall.com/cards/named",headers=headers,params={"fuzzy":text, "pretty":True})
+
+            #if a 404 is returned the card was not found
             if response.status_code == 404:
                 print("card name invalid, try getting a clearer picture or ensure that your OCR is setup for the language the card is in")
+
             else:
                 cardDat=response.json()
                 legalities=cardDat["legalities"]
